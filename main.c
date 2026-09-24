d138ee6d61ae9c9c6b6292fcc4c16bf50d82ed17
Gimhan Nethsara <as20250477@sci.sjp.ac.lk>
2026-09-25 01:11:34 +0530

    Add patient parallel arrays

--- main.c
+++ main.c
@@ -19,3 +19,21 @@ char wardName[NUM_WARDS][30]  = {"General Ward","Paediatric Ward",
 float wardDailyRate[NUM_WARDS] = {3000.00,6000.00,12000.00,25000.00};
 int wardCapacity[NUM_WARDS]    = {20,10,10,5};
 int bedOccupancy[NUM_WARDS][20] = {0};
+
+char patientName[MAX_PATIENTS][50];
+int patientAge[MAX_PATIENTS];
+int urgencyLevel[MAX_PATIENTS];
+int specialtyChoice[MAX_PATIENTS];
+int isAdmitted[MAX_PATIENTS];
+int wardChoice[MAX_PATIENTS];
+int daysAdmitted[MAX_PATIENTS];
+int bedNunber[MAX_PATIENTS];
+
+float waitTime[MAX_PATIENTS];
+float surcharge[MAX_PATIENTS];
+float wardCost[MAX_PATIENTS];
+float grossTotal[MAX_PATIENTS];
+float discount[MAX_PATIENTS];
+float finalAmount[MAX_PATIENTS];
+
+int patientCount = 0;
